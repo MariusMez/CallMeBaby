1e76dcdf6bc2472b7c015bccb8029b0e9e0d1c46
Marius <marius.mez@gmail.com>
2017-09-24 21:28:17 +0200

    Switch to cherrypy

--- callmebaby.py
+++ callmebaby.py
@@ -1,43 +1,53 @@
 import configargparse
-from flask import Flask
+import cherrypy
 from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse, Say
-
-app = Flask(__name__)
+from cherrypy.process.plugins import Daemonizer
+d = Daemonizer(cherrypy.engine)
+d.subscribe()
 
 p = configargparse.ArgParser(default_config_files=['./config.txt'])
 p.add('-c', '--config', required=False, is_config_file=True, help='config file path')
 p.add('--account', required=True, help='Twilio account SID')  
 p.add('--auth', required=True, help='Twilio authentication token')  
 p.add('--number', required=True, help='Twilio Number start with +3')  
-p.add('-to', required=True, help='Phone to call : eg. +33XXXXXXXXX')  
+p.add('-to', required=False, help='Phone to call : eg. +33XXXXXXXXX')  
 
 args = p.parse_args()
 if not args.account:
     exit(parser.print_usage())
 
-@app.route("/guigui/content", methods=['GET', 'POST'])
-def content():
-    response = VoiceResponse()
-    response.say('Salut jeune Géocacheur !', voice='alice', language='fr-FR')
-    response.play('http://demo.twilio.com/hellomonkey/monkey.mp3')
+class CallMeBaby(object):
+    @cherrypy.expose
+    def content(self, url, **params):
+        response = VoiceResponse()
+        #response.say('Salut jeune Géocacheur !', voice='alice', language='fr-FR')
+        #response.play('http://veroguigui83.free.fr/ValEvent/message-la-source.mp3')
+        print(str(url))
+        response.play(str(url))
+        return str(response)
 
-    return str(response)
-
-@app.route("/guigui/call")
-def call():
-
-    # Your Account Sid and Auth Token from twilio.com/user/account
-    account_sid = args.account
-    auth_token = args.auth
-    client = Client(account_sid, auth_token)
-    call = client.calls.create(
-        to=args.to,
-        from_=args.number,
-        url="http://www.mysolar.io/guigui/content",
-        method='GET'
-    )
-    return call.sid
+    @cherrypy.expose
+    def call(self, num, url, **params):
+        # Your Account Sid and Auth Token from twilio.com/user/account
+        account_sid = args.account
+        auth_token = args.auth
+        number = args.number
+        url_xml = "http://www.mysolar.io/callmebaby/content?url="+str(url)
+        print(url_xml)
+        client = Client(account_sid, auth_token)
+        call = client.calls.create(
+            to=num,
+            from_=number,
+            url=url_xml,
+            method='GET'
+        )
+        return call.sid
 
 if __name__ == '__main__':
-    app.run()
+    cherrypy.config.update({
+        'server.socket_port': 5000,
+        'tools.proxy.on': True,
+        'tools.proxy.base': 'http://www.mysolar.io'
+    })
+    cherrypy.quickstart(CallMeBaby(),'/callmebaby')
